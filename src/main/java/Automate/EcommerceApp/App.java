9b3613eefcd34225a19354ccd174a088f54e0eab
Ashutosh Kumar <ashukmr919@gmail.com>
2022-10-19 03:18:49 +0530

    commit

---
+++ src/main/java/Automate/EcommerceApp/App.java
@@ -0,0 +1,13 @@
+package Automate.EcommerceApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
